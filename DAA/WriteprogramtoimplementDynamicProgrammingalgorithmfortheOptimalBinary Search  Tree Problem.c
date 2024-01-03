63b265dc9fff0e77b4f6fb9ed8631b2e9257a112
kishan talekar <115180515+kishantalekar@users.noreply.github.com>
2024-01-03 12:00:26 +0530

    Create WriteprogramtoimplementDynamicProgrammingalgorithmfortheOptimalBinary Search  Tree Problem.c

---
+++ DAA/WriteprogramtoimplementDynamicProgrammingalgorithmfortheOptimalBinary Search  Tree Problem.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int optimalBSTCost(int *freq, int n) {
+    int cost[n][n];
+
+    for (int i = 0; i < n; i++) cost[i][i] = freq[i];
+
+    for (int L = 2; L <= n; L++) {
+        for (int i = 0; i <= n - L; i++) {
+            int j = i + L - 1;
+            cost[i][j] = INT_MAX;
+            for (int r = i; r <= j; r++) {
+                int c = ((r > i) ? cost[i][r-1] : 0) + ((r < j) ? cost[r+1][j] : 0) + freq[r];
+                if (c < cost[i][j]) cost[i][j] = c;
+            }
+        }
+    }
+    return cost[0][n - 1];
+}
+
+int main() {
+    int n;
+    printf("Enter the number of keys: ");
+    scanf("%d", &n);
+
+    int *freq = malloc(n * sizeof(int));
+    printf("Enter frequencies:\n");
+    for (int i = 0; i < n; i++) scanf("%d", &freq[i]);
+
+    printf("Optimal BST cost: %d\n", optimalBSTCost(freq, n));
+    free(freq);
+    return 0;
+}
